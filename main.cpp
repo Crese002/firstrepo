1a7598585a93ad3d83de262bddf8fdf599caca07
Cole Resetco <crese002@hammer.cs.ucr.edu>
2016-04-11 01:19:31 -0700

    Version 1 of main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//test comment for vi vs vim purposes
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello world, how are you today? you havent answered the last 3 times I asked, but maybe you will this time." << std::endl;
+	return 1;
+}
+
