89c0dbf22581a5d18b8d0253b4fe324ecb5aecca
Cole Resetco <crese002@hammer.cs.ucr.edu>
2016-04-11 01:38:44 -0700

    added user input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 //test comment for vi vs vim purposes
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-	std::cout << "hello world, how are you today? you havent answered the last 3 times I asked, but maybe you will this time." << std::endl;
+	std::string name;
+	std::cout << "what's yer name boi?" << std::endl;
+	std::cin >> name;
+	std::cout << "hello " << name << ", how are you today? you havent answered the last 3 times I asked, but maybe you will this time." << std::endl;
+	
 	return 1;
 }
 
